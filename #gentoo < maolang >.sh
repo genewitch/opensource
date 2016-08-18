e7dc2c4012a81c9b789ba7910c923f77ff0aa8c9
genewitch <genewitch@gmail.com>
2016-08-18 01:07:08 -0500

    Update #gentoo < maolang >.sh

--- #gentoo < maolang >.sh
+++ #gentoo < maolang >.sh
@@ -1,5 +1,8 @@
 #gentoo < maolang >
 {
-make alias ='inhere'
-qlop -t $(qlist -IC) | sort -nk2 }
-
+#make alias ='inhere'
+#qlop -t $(qlist -IC) | sort -nk2 }
+#
+#Literal command line:
+alias emergestats='sudo qlop -tgH $(qlist -IC) | sort -nk2'
+}
