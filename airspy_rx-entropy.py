4fa59ea57cd369635fae593dc6885c25dd63a4a2
genewitch <genewitch@gmail.com>
2018-03-16 15:24:55 -0500

    Create airspy_rx-entropy.py

---
+++ airspy_rx-entropy.py
@@ -0,0 +1,27 @@
+import sys
+import numpy as np
+import struct
+#even = True
+#with open("rndbits", "rb") as f:
+ar = np.empty(1, dtype="int16")
+
+while True:
+  #c = f.read(2)
+  i, = struct.unpack('h', sys.stdin.buffer.read(2))
+  print(i, end=",")
+  ar =  np.append(ar,i)
+  if ar.size % 1000 == 0:
+    sys.stderr.write(str(np.median(ar)))
+    sys.stderr.write("\n")
+    sys.stderr.write(str(np.mean(ar)))
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+#s = sys.stdin.read(2)
+#a = np.loadtxt(s, dtype=np.int16)
+#print (np.median(a))
+#print (np.mean(a))
+#print (np.amin(a))
+#print (np.amax(a))
+
+
+#print (a)
