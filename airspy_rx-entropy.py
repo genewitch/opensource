4f9a713ac28fe641972334b8fb0fd12af7315f81
genewitch <genewitch@gmail.com>
2018-03-18 13:49:51 -0500

    Update airspy_rx-entropy.py
    commenting.

--- airspy_rx-entropy.py
+++ airspy_rx-entropy.py
@@ -1,15 +1,22 @@
+# take airspy_rx ype 2 (signed, 16 bit integers between -4096 and +4096)
+# and output debiased raw binary (packed into bytes)
+
 import sys
-import numpy as np
 import struct
+
+
 while True:
-  
+  #use a list to build a byte
   bild = []
 
   while len(bild) < 8:
+    
+    #read three int16 from stdin
     i, = struct.unpack('h', sys.stdin.buffer.read(2))
     j, = struct.unpack('h', sys.stdin.buffer.read(2))
     k, = struct.unpack('h', sys.stdin.buffer.read(2))
     
+    #drop if anything all or anything is equal
     if i == j or i == k or k == j:
       continue
     
@@ -24,13 +31,18 @@ while True:
     else:
       continue
   
-  outa = ''.join(bild)
+  outa = ''.join(bild) #this makes an 8 character string comprised of 1s and/or 0s
+  
+  #  debug
   #sys.stderr.write(str(outa))
   #sys.stderr.write("\n")
-  outi = int(outa,2)
+  
+  outi = int(outa,2) # take the binary string and make it an 8 bit int
+  
+  #  debug
   #sys.stderr.write(str(outi))
   #sys.stderr.write("\n")
-  sys.stderr.flush()
-  outb = outi.to_bytes(1,byteorder="little",signed=False)
-  sys.stdout.buffer.write(outb)
-  sys.stdout
+  #sys.stderr.flush()
+  
+  outb = outi.to_bytes(1,byteorder="little",signed=False) #take the integer and make it a raw byte
+  sys.stdout.buffer.write(outb) #write raw byte to stdout
