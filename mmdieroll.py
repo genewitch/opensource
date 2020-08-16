75c84fb89bef25793e0794430d48f0e43183e983
genewitch <genewitch@gmail.com>
2020-08-16 14:10:53 -0500

    Create mmdieroll.py

---
+++ mmdieroll.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os
+import random
+from urllib import parse
+print ("Content-type: application/json")
+print ( "" )
+qs = parse.parse_qs(os.environ['QUERY_STRING'],keep_blank_values=True)
+query = qs['text'][0]
+rolls = [0, 0]
+substring = "d"
+if substring not in query.lower():
+    rolls[0]=1
+    rolls[1]=6
+    query="1d6"
+else:
+    rolls = query.lower().split('d')
+
+#query="2d20"
+min = 1
+b = 6
+
+print ('{"response_type": "in_channel", "text": "rolled ' + query + ':', end='')
+
+for die in range (0,int(rolls[0])):
+    print(' ' + str(random.randint(min, int(rolls[1]))), end='')
+
+    #print(random.randint(a, int(rolls[1])))
+
+print ('", "username" : "roller"}')
