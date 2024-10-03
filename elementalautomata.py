1fb5b384790851b7dae0ed01afbed9eb32240dc6
genewitch <genewitch@gmail.com>
2024-10-03 04:37:31 -0500

    Update elementalautomata.py
    added prompt and using the "N" key to change numbers

--- elementalautomata.py
+++ elementalautomata.py
@@ -1,19 +1,9 @@
-# see https://mathworld.wolfram.com/ElementaryCellularAutomaton.html
-# usage python elementalautomata.py number
-# where number is an 8 bit integer of the rule you want to use.
-# 45, 110, 167 are decent examples.
+# i had to fix a lot and this does not check input
+# i am debating trying to make it faster without being
+# clever
 import pygame
 from secrets import randbelow
 import sys
-
-# Check if there are enough arguments
-if len(sys.argv) != 2:
-    print("Usage: python pyprogram.py <number>")
-    sys.exit(1)
-
-# Get the input number from the command line argument
-input_num = int(sys.argv[1])
-print(f"Input number is: {input_num}")
 
 pygame.init()
 oneD = []
@@ -22,85 +12,110 @@ w, h = 640, 640
 ScrSize = (w, h)
 Gray = (200, 200, 200)
 screen = pygame.display.set_mode(ScrSize)
+pygame.display.set_caption("Scrolling Lines")
 world = pygame.Surface((ScrSize[0], ScrSize[1]))
 world.fill(Gray)
+font = pygame.font.Font(None, 36)
 
-# initial state
-for n in range(0, w+2):
-    oneD.append(randbelow(2))
+def get_input_num():
+    user_input = ""
+    input_active = True
 
-for n in range(0, w+2):
-    lineOut.append(0)
+    while input_active:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    input_active = False
+                elif event.key == pygame.K_BACKSPACE:
+                    user_input = user_input[:-1]
+                else:
+                    user_input += event.unicode
 
-print("initial state done")
+        # Render input text
+        prompt = font.render("Enter a rule number:", True, (255, 255, 255))
+        screen.blit(prompt, (20, 20))
+        input_box = font.render(user_input, True, (255, 255, 255))
+        screen.blit(input_box, (300, 20))
+        pygame.display.flip()
+    
+    return int(user_input)
 
+# Function to convert integer to binary list
 def int_to_binary_list(n, length=8):
-    # Convert integer to binary string, remove the "0b" prefix, and pad with leading zeros
     bin_str = bin(n)[2:].zfill(length)
-    # Convert binary string to a list of integers
     bin_list = [int(digit) for digit in bin_str]
     return bin_list
-
-# Example usage
-#input_num = 110
-rules = int_to_binary_list(input_num, 8)
-print("rule #" + str(input_num))
-
-
-#rules = [0,0,0,1,1,1,1,0] #30
-#rules = [0,	1,	0, 	1, 	1, 	0, 	1, 	0] #90
-#rules = [ 0,1,1,0,1,1,1,0 ] # 110
-#rules = [ 0,1,1,0,1,1,1,1 ] # 111
 
 def scroll_world():
     global world
     world.scroll(dy=-1)
     pygame.draw.line(world, Gray, (0, h - 1), (w, h - 1))
 
+def reset_world():
+    global oneD, lineOut, world
+    oneD = [randbelow(2) for _ in range(w + 2)]
+    lineOut = [0 for _ in range(w + 2)]
+    world.fill(Gray)
+
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
+    input_num = get_input_num()
+    rules = int_to_binary_list(input_num, 8)
+    reset_world()
+    print("rule #" + str(input_num))
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
+                elif event.key == pygame.K_n:
+                    print("caught 'n'")
+                    
+                    input_num = get_input_num()
+                    rules = int_to_binary_list(input_num, 8)
+                    reset_world()
+                    break
 
-    for row in range(0, h):
-        for t in range(1, w+1):
-            if oneD[t-1:t+2] == [1, 1, 1]:
-                lineOut[t] = rules[0]
-            elif oneD[t-1:t+2] == [1, 1, 0]:
-                lineOut[t] = rules[1]
-            elif oneD[t-1:t+2] == [1, 0, 1]:
-                lineOut[t] = rules[2]
-            elif oneD[t-1:t+2] == [1, 0, 0]:
-                lineOut[t] = rules[3]
-            elif oneD[t-1:t+2] == [0, 1, 1]:
-                lineOut[t] = rules[4]
-            elif oneD[t-1:t+2] == [0, 1, 0]:
-                lineOut[t] = rules[5]
-            elif oneD[t-1:t+2] == [0, 0, 1]:
-                lineOut[t] = rules[6]
-            elif oneD[t-1:t+2] == [0, 0, 0]:
-                lineOut[t] = rules[7]
-            else:
-                print("BONKERS")
-                
-        #if row % 50 == 0:
-        #    print(".", end='')
+        for row in range(0, h):
+            for t in range(1, w+1):
+                if oneD[t-1:t+2] == [1, 1, 1]:
+                    lineOut[t] = rules[0]
+                elif oneD[t-1:t+2] == [1, 1, 0]:
+                    lineOut[t] = rules[1]
+                elif oneD[t-1:t+2] == [1, 0, 1]:
+                    lineOut[t] = rules[2]
+                elif oneD[t-1:t+2] == [1, 0, 0]:
+                    lineOut[t] = rules[3]
+                elif oneD[t-1:t+2] == [0, 1, 1]:
+                    lineOut[t] = rules[4]
+                elif oneD[t-1:t+2] == [0, 1, 0]:
+                    lineOut[t] = rules[5]
+                elif oneD[t-1:t+2] == [0, 0, 1]:
+                    lineOut[t] = rules[6]
+                elif oneD[t-1:t+2] == [0, 0, 0]:
+                    lineOut[t] = rules[7]
+                else:
+                    print("BONKERS")
+                    
+            for pixel in range(1, w):
+                if lineOut[pixel] == 1:
+                    world.set_at((pixel, h-1), (0, 0, 0))
+                else:
+                    world.set_at((pixel, h-1), (255, 255, 255))
+            
+            lineOut[0] = oneD[w]
+            lineOut[w] = oneD[0]
+            oneD = list(lineOut)
 
-        for pixel in range(1, w):
-            if lineOut[pixel] == 1:
-                world.set_at((pixel, h-1), (0, 0, 0))
-            else:
-                world.set_at((pixel, h-1), (255, 255, 255))
+            scroll_world()
         
-        lineOut[0] = oneD[w]
-        lineOut[w] = oneD[0]
-        oneD = list(lineOut)
-
-        scroll_world()
         screen.blit(world, (0, 0))
         pygame.display.update()
         
